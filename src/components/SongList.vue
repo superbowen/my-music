7d3226886e2f3a76658d675a90c4d7c725c21527
博文 <z.bowen66@gmail.com>
2017-04-09 15:25:09 +0800

    增加列表动画

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="song-list">
+  <div class="song-list" :class="aniClass">
     <div class="song" v-for="(song, index) in songs" @click="selectSong(song)">
-      <mu-badge :content="(index+1)+' '" circle></mu-badge>
+      <mu-badge v-if="showBadge" :content="(index+1)+' '" circle></mu-badge>
       <span class="title">{{song.songname}}</span>
       <span class="singer">{{song.singer[0].name}}</span>
     </div>
@@ -14,13 +14,31 @@
       songs: {
         type: Array,
         required: true
+      },
+      showBadge: {
+        type: Boolean,
+        default: true
       }
+    },
+    data() {
+      return {
+        aniClass: 'ani1'
+      }
+    },
+    watch: {
+      'songs': 'fresh'
     },
     methods: {
       selectSong (song) {
         console.log(song)
         this.$store.commit('selectSong', song)
         this.$router.push('/player')
+      },
+      fresh() {
+        this.aniClass = 'hide'
+        setTimeout(() => {
+          this.aniClass = 'ani1'
+        }, 300)
       }
     }
   }
@@ -37,21 +55,38 @@
       white-space: nowrap
       overflow: hidden
       text-overflow: ellipsis
+      &:last-child
+        border none
       .title
         margin-left: 1.5em
         white-space: nowrap
         overflow: hidden
         text-overflow: ellipsis
       .singer
-        font-size: 0.7em
+        font-size: 0.6em
         margin-left: .7em;
         white-space: nowrap
         overflow: hidden
         text-overflow: ellipsis
 
-
   .hide
     display: none
+
+  .ani1
+    for i in 1..15
+      .song:nth-child({i})
+        animation-name: animationStyle1
+        animation-duration: 200ms * i
+        animation-timing-function: ease-in-out
+        animation-fill-mode: forwards
+
+  @keyframes animationStyle1
+    0%
+      opacity: .1;
+      transform: translate3d(500px, 100px, 0)
+    100%
+      opacity: 1;
+      transform: translate3d(0, 0, 0)
 
 
 </style>
